81f0893ca598f81107b443d17fbc9ed14bce7169
Matthew Fay <mmfay3@gmail.com>
2026-03-11 03:24:16 -0700

    Add Footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+
+	return (
+		<footer className="px-6 py-8 text-center text-xs text-slate-500 dark:text-slate-400">
+			© {new Date().getFullYear()} NutriGym. A Softwarerror product.
+		</footer>
+	);
+
+}
